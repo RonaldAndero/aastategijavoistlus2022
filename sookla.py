3ebc8480f48aa017473ead8b8b9e2e013c51b9df
RonaldAndero <94618048+RonaldAndero@users.noreply.github.com>
2022-03-08 14:21:30 +0200

    file1
    uuendus 2

--- sookla.py
+++ sookla.py
@@ -8,17 +8,15 @@ url = "http://192.168.22.172/menu-example/"
 sisu = requests.get(url)
 doc = BeautifulSoup(sisu.text, "html.parser")
 
-
-
 # for loop mis leiab pealkirjad, nimetused, hinnad ja lisainfod
 pealkirjad = doc.select('strong')
 for pealkiri in pealkirjad:
     tabel = pealkiri.findNext("ul")
     if tabel:
-        print(pealkiri.text)
         asi = tabel.find_all("li")
         for rida in asi:
             stats = [
                 (i.contents[0], i.contents[1].text, i.contents[3].text) for i in rida
-            ]
-            print(stats[0][0])
+           ]
+    data = {"nimetus":pealkiri.text, "toidud": [{"nimetus": stats[0][0], "hind": stats[0][1], "lisainfo": stats[0][2]}]}
+    print(data)
