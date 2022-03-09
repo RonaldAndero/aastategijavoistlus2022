b899dd374d322568cbeba0a2e16e7341176c62bb
RonaldAndero <94618048+RonaldAndero@users.noreply.github.com>
2022-03-09 09:22:15 +0200

    file1
    Viimane versioon. vist

--- sookla.py
+++ sookla.py
@@ -2,21 +2,45 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-
 url = "http://192.168.22.172/menu-example/"
 
-sisu = requests.get(url)
-doc = BeautifulSoup(sisu.text, "html.parser")
 
-# for loop mis leiab pealkirjad, nimetused, hinnad ja lisainfod
+data = requests.get(url)
+doc = BeautifulSoup(data.text, "html.parser")
+
+#info jaoks list
+jsonlist = []
+
+#main kood
+iterator = 0
 pealkirjad = doc.select('strong')
+#vaatame pealkirja järgi järest
 for pealkiri in pealkirjad:
+    #leiame iga ul sourcist
     tabel = pealkiri.findNext("ul")
+    try:
+        tabel2 = tabel.find_next_sibling()
+        if tabel2 != None:
+            tabel.append(tabel2)
+    except:
+        pass
+    #kui leiame
     if tabel:
+        #li sees on info
         asi = tabel.find_all("li")
+        jsonlist.append({"nimetus":pealkiri.text, "toidud": []})
+        #saame selle info siin
         for rida in asi:
+            #kiire loop mis filtreerib infot mida meil vaja
             stats = [
                 (i.contents[0], i.contents[1].text, i.contents[3].text) for i in rida
            ]
-    data = {"nimetus":pealkiri.text, "toidud": [{"nimetus": stats[0][0], "hind": stats[0][1], "lisainfo": stats[0][2]}]}
-    print(data)
+            #lükkame info listi
+            jsonlist[iterator]["toidud"].append({"nimetus": stats[0][0], "hind": stats[0][1].replace("€", ""), "lisainfo": stats[0][2]})
+        iterator += 1
+
+#teeme jsoni
+prettyjson = json.dumps(jsonlist, indent=4)
+f = open("menu.json", "w" )
+f.write(prettyjson)
+f.close()
